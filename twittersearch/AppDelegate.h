05b8511e371ce1c5d3e6ef1c5b8c6ebad8189b9e
Chris <crookiemoonster+github@gmail.com>
2013-01-15 13:42:08 -0500

    Initial Commit

---
+++ twittersearch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  twittersearch
+//
+//  Created by crkmnstr on 1/15/13.
+//  Copyright (c) 2013 evilBlue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
